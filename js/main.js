b640aa9d440e4b6d6d5799ca1e72905ae7492391
Sheina Mompremier <sheina.sally@gmail.com>
2015-02-01 21:37:42 -0500

    added presidents

--- js/main.js
+++ js/main.js
@@ -1,2 +1,46 @@
 var iceCream = ["Strawberry", "Vanilla", "Cookies and Cream"];
 iceCream.push("Cookie Dough");  
+
+var President1 = {
+firstName: "Barak",
+lastName: "Obama",
+termLength: 2,
+party: "Democrat",
+years: 8,
+}
+
+var President2 = {
+firstName: "George W.",
+lastName: "Bush",
+termLength: 2,
+party: "Republican",
+years: 8,
+}
+
+var President3 = {
+firstName: "Bill",
+lastName:"Clinton",
+termLength:2,
+party:"Democrat",
+years:8, 
+}
+
+var President4 = {
+firstName: "George H.",
+lastName:"Bush",
+termLength:2,
+party:"Republican",
+years:8,
+}
+
+var President5 = {
+firstName: "Ronald",
+lastName:"Reagon",
+termLength:2,
+party:"Republican",
+years:8,
+}
+
+var Presidents = [President1, President2, President3, President4, President5];
+
+console.log (President3.firstName, President3.lastName);
