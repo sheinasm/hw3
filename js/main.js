163984b65e1cf93cb813977a2756f2dcf31e9f36
Richard Kalehoff <richardkalehoff@gmail.com>
2015-02-03 11:07:48 -0500

    declare 'numba' function

--- js/main.js
+++ js/main.js
@@ -51,3 +51,13 @@ function howdy() {
 }
 
 howdy();
+
+var numba = function( string ) {
+    if (string.length < 7) {
+        console.log( "What a short little word!" );
+    } else if (string.length > 7) {
+        console.log( "I'm sorry, but that’s too many to count." );
+    } else {
+        console.log( "7, what a perfect choice!" );
+    }
+}
